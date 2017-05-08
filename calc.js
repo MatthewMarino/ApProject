063af190076f7eb8ef052b202ec75a4138e24fba
Matthew Marino <Matthew Marino>
2017-05-07 21:50:16 -0700

    function

--- calc.js
+++ calc.js
@@ -16,6 +16,28 @@ var D = 500;
 var M = 1000;
 
 
+//figure this out
+/*
+function convertToRoman(num) {
+
+  var decimal = [ 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 ];
+  var romanNumeral = [ 'M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I' ];
+
+  var done = '';
+
+  for (var i = 0; i < decimal.length; i++) {
+    while (decimal[i] <= num) {
+      done += romanNumeral[i];
+      num -= decimal[i];
+    }
+  }
+
+  return done;
+}
+*/
+
+
+
 // Helper function for displaying the current input
 function inputlimit() {
     if (inputlength > 11) {
